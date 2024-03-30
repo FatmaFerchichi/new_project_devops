e9e0676d7ad43e5ae0049c178b786eafd667eb6b
FatmaFerchichi <135468163+FatmaFerchichi@users.noreply.github.com>
2024-03-30 03:26:31 +0100

    testttt

--- src/test/java/tn/esprit/devops_project/services/OperatorServiceImplTest.java
+++ src/test/java/tn/esprit/devops_project/services/OperatorServiceImplTest.java
@@ -1,11 +1,7 @@
 package tn.esprit.devops_project.services;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import tn.esprit.devops_project.entities.Operator;
@@ -13,18 +9,17 @@ import tn.esprit.devops_project.repositories.OperatorRepository;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-
 @ActiveProfiles("test")
 class OperatorServiceImplTest {
 
-    @Mock
+    @Autowired
     private OperatorRepository operatorRepository;
 
-    @InjectMocks
+    @Autowired
     private OperatorServiceImpl operatorService;
 
     @Test
@@ -35,8 +30,8 @@ class OperatorServiceImplTest {
         operator1.setFname("Ferchichi");
         operator1.setLname("Fatma");
 
-        // Mock repository behavior
-        when(operatorRepository.findById(1L)).thenReturn(Optional.of(operator1));
+        // Save the mock data to the repository
+        operatorRepository.save(operator1);
 
         // Act
         Operator retrievedOperator = operatorService.retrieveOperator(1L);
@@ -45,24 +40,5 @@ class OperatorServiceImplTest {
         assertNotNull(retrievedOperator);
         assertEquals(1L, retrievedOperator.getIdOperateur());
         assertEquals("Ferchichi", retrievedOperator.getFname()); // Compare with the expected value
-
-        // Verify that findById method was called exactly once with the correct argument
-        Mockito.verify(operatorRepository, Mockito.times(1)).findById(1L);
-    }
-    @Test
-    void testDeleteOperator() {
-        // Mock data
-        Long idToDelete = 1L;
-        Operator operatorToDelete = new Operator();
-        operatorToDelete.setIdOperateur(idToDelete);
-
-        // Mock repository behavior
-        lenient().when(operatorRepository.findById(idToDelete)).thenReturn(Optional.of(operatorToDelete));
-
-        // Call the method to be tested
-        operatorService.deleteOperator(idToDelete);
-
-        // Verify that deleteById method was called exactly once with the correct argument
-        Mockito.verify(operatorRepository, Mockito.times(1)).deleteById(idToDelete);
     }
 }
